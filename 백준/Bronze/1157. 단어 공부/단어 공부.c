9e1a2adcc2c689402a2def067e830eb720e4889c
chanwon05 <165672373+chanwon05@users.noreply.github.com>
2024-05-27 19:56:42 +0900

    [Bronze I] Title: 단어 공부, Time: 16 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.c
+++ 백준/Bronze/1157. 단어 공부/단어 공부.c
@@ -1,56 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main(void)
 {
-	char word[1000000] = { 0, };
-	scanf("%s", word);
-	int count[26] = { 0, };
-	
-	for (char i = 'a'; i <= 'z'; i++)
-	{
-		for (int j = 0; j < strlen(word); j++)
-		{
-			if (word[j] == i)
-			{
-				count[word[j] - 'a'] += 1;
-			}
-		}
-	}
+    char word[1000000];
+    scanf("%s", word);
+    int count[26] = { 0 };
+    int len = strlen(word);
+    
+    for (int j = 0; j < len; j++)
+    {
+        if (word[j] >= 'a' && word[j] <= 'z')
+        {
+            count[word[j] - 'a'] += 1;
+        }
+        else if (word[j] >= 'A' && word[j] <= 'Z')
+        {
+            count[word[j] - 'A'] += 1;
+        }
+    }
 
-	for (char i = 'A'; i <= 'Z'; i++)
-	{
-		for (int j = 0; j < strlen(word); j++)
-		{
-			if (word[j] == i)
-			{
-				count[word[j] - 'A'] += 1;
-			}
-		}
-	}
+    char n;
+    int max = 0;
 
-	char n;
-	int max = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (count[i] > max)
+        {
+            max = count[i];
+            n = 'A' + i;
+        }
+        else if (count[i] == max)
+        {
+            n = '?';
+        }
+    }
 
-	for (int i = 0; i < 26; i++)
-	{
-		if (count[i] > max)
-		{
-			max = count[i];
-			n = 65 + i;
-		}
-		else if (count[i] == max)
-		{
-			n = 63;
-		}
-	}
+    printf("%c\n", n);
 
-	
-
-	printf("%c", n);
-
-
-
-	return 0;
+    return 0;
 }
