39d2141cd4c09b4ab1117d4c605d6c254b32a7b3
chanwon05 <165672373+chanwon05@users.noreply.github.com>
2025-01-10 17:33:50 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 7760 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[1000001] = {0 };
+	bool occur[2000001];
+	int n, x;
+
+	int ans = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	cin >> x;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (x - arr[i] > 0 && occur[x - arr[i]]) ans++;
+		occur[arr[i]] = true;
+	}
+
+	cout << ans;
+
+	return 0;
+}
